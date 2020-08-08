370103bdc991a7db8af6d53eb6bfc09d4c6e3926
d5uke <d5uke.cmd@gmail.com>
2020-08-08 21:20:15 +0900

    add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  before_action :require_login
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
+  end
 end
