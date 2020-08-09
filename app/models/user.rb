377e159e7dc9e1b3efdae9f35b350dd663e3fa41
d5uke <d5uke.cmd@gmail.com>
2020-08-09 09:30:18 +0900

    validate users_limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  belogs_to :client, optional: true # 影山さんはnull
+  belongs_to :client, optional: true # 影山さんはnull
   # belongs_to :department, optional: true # 影山さんはnull
   # has_many :daily_reports, dependent: :restrict_with_exception
   # has_many :arigatonas, dependent: :restrict_with_exception
@@ -15,6 +15,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :password, confirmation: true, length: { minimum: 8 }, if: -> { new_record? || changes[:password_digest] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:password_digest] }
+  validate :over_users_limit?
 
   enum role: { system_admin: 10, client_admin: 20, client_general: 30 }
+
+  def over_users_limit?
+    errors.add(:base, 'ユーザ数が上限に達しています') if client && client.users.count >= client.users_limit
+  end
 end
