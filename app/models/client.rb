6204f1f104baa9e947c5f2387b09064f41d54ada
d5uke <d5uke.cmd@gmail.com>
2020-08-09 10:50:55 +0900

    set associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,9 +2,9 @@
 
 class Client < ApplicationRecord
   has_many :users, dependent: :restrict_with_exception
-  # has_many :departments, dependent: :restrict_with_exception
-  # has_many :work_contents, dependent: :restrict_with_exception
-  # has_many :work_properties, dependent: :restrict_with_exception
+  has_many :departments, dependent: :restrict_with_exception
+  has_many :work_contents, dependent: :restrict_with_exception
+  has_many :work_properties, dependent: :restrict_with_exception
 
   validates :name, presence: true
 end
--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -2,6 +2,6 @@
 
 class DailyReport < ApplicationRecord
   belongs_to :user
-  # has_many :works, dependent: :restrict_with_exception
-  # has_one :arigatona, dependent: :restrict_with_exception
+  has_many :works, dependent: :restrict_with_exception
+  has_one :arigatona, dependent: :restrict_with_exception
 end
