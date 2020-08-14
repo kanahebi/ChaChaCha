7a00a0ecada1f85aaa344b38a4f60e36b2f1e3b2
Daisuke Tsuji <dim0627@gmail.com>
2020-08-14 11:48:21 +0900

    remove extra codes.

--- app/policies/api/user_policy.rb
+++ app/policies/api/user_policy.rb
@@ -2,10 +2,4 @@ class Api::UserPolicy < ApplicationPolicy
   def index?
     true
   end
-
-  class Scope < Scope
-    def resolve
-      scope.where(department_id: user.department_id).all
-    end
-  end
 end
