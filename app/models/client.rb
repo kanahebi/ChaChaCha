efd591a1ec151aa41f49d8597abb38f4742c31f2
d5uke <d5uke.cmd@gmail.com>
2020-08-15 12:49:43 +0900

    validate users_limit

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,5 +6,6 @@ class Client < ApplicationRecord
   has_many :work_contents, dependent: :restrict_with_exception
   has_many :work_properties, dependent: :restrict_with_exception
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :users_limit, numericality: { greater_than_or_equal_to: 0 }
 end
