e3d70ebbde820e7ad5f1f49696340f4f34b9aac3
d5uke <d5uke.cmd@gmail.com>
2020-08-15 11:30:47 +0900

    system_admin_client#index

--- app/controllers/system_admin/clients_controller.rb
+++ app/controllers/system_admin/clients_controller.rb
@@ -1,74 +1,63 @@
+# frozen_string_literal: true
+
 class SystemAdmin::ClientsController < ApplicationController
-  before_action :set_system_admin_client, only: [:show, :edit, :update, :destroy]
+  before_action :set_client, only: [:edit, :update, :destroy]
 
-  # GET /system_admin/clients
-  # GET /system_admin/clients.json
   def index
-    @system_admin_clients = SystemAdmin::Client.all
+    authorize [:system_admin, :client]
+
+    @clients = Client.all
   end
 
-  # GET /system_admin/clients/1
-  # GET /system_admin/clients/1.json
-  def show
-  end
-
-  # GET /system_admin/clients/new
   def new
-    @system_admin_client = SystemAdmin::Client.new
+    authorize [:system_admin, :client]
+
+    @client = Client.new
   end
 
-  # GET /system_admin/clients/1/edit
   def edit
+    authorize [:system_admin, @client]
   end
 
-  # POST /system_admin/clients
-  # POST /system_admin/clients.json
   def create
-    @system_admin_client = SystemAdmin::Client.new(system_admin_client_params)
+    authorize [:system_admin, :client]
 
-    respond_to do |format|
-      if @system_admin_client.save
-        format.html { redirect_to @system_admin_client, notice: 'Client was successfully created.' }
-        format.json { render :show, status: :created, location: @system_admin_client }
-      else
-        format.html { render :new }
-        format.json { render json: @system_admin_client.errors, status: :unprocessable_entity }
-      end
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to edit_system_admin_client_url(@client), notice: t("helpers.done.create")
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /system_admin/clients/1
-  # PATCH/PUT /system_admin/clients/1.json
   def update
-    respond_to do |format|
-      if @system_admin_client.update(system_admin_client_params)
-        format.html { redirect_to @system_admin_client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @system_admin_client }
-      else
-        format.html { render :edit }
-        format.json { render json: @system_admin_client.errors, status: :unprocessable_entity }
-      end
+    authorize [:system_admin, @client]
+
+    if @client.update(client_params)
+      redirect_to edit_system_admin_client_url, notice: t("helpers.done.update")
+    else
+      render :edit
     end
   end
 
-  # DELETE /system_admin/clients/1
-  # DELETE /system_admin/clients/1.json
   def destroy
-    @system_admin_client.destroy
-    respond_to do |format|
-      format.html { redirect_to system_admin_clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    authorize [:system_admin, @client]
+
+    @client.destroy
+    redirect_to system_admin_clients_url, notice: t("helpers.done.destroy")
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_system_admin_client
-      @system_admin_client = SystemAdmin::Client.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def system_admin_client_params
-      params.fetch(:system_admin_client, {})
-    end
+  def set_client
+    @client = Client.find(params[:id])
+  end
+
+  def client_params
+    params.require(:client).permit(
+      :name,
+      :users_limit
+    )
+  end
 end
