286c959dc3a4b39686805a9ffdebb4d1b8909d61
d5uke <d5uke.cmd@gmail.com>
2020-08-15 11:48:39 +0900

    judge root path by role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def root_path_judged_by_role
+    case current_user.role
+    when 'system_admin'
+      system_admin_root_url
+    when 'client_admin'
+      # TODO: クライアント管理者用画面作ったらそっちにする
+      root_url
+    when 'client_general'
+      root_url
+    end
+  end
 end
