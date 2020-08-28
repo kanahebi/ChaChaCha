fc4602b9627f9436025b17a8ac75734f76e59819
d5uke <d5uke.cmd@gmail.com>
2020-08-28 21:07:04 +0900

    fix tests

--- spec/requests/client_admin/work_contents_spec.rb
+++ spec/requests/client_admin/work_contents_spec.rb
@@ -3,43 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe "/client_admin/work_contents", type: :request do
-  let!(:work_content) { create :work_content }
+  before { login(user) }
+
+  let!(:user) { create(:user, :client_admin) }
+  let!(:work_content) { create :work_content, client_id: user.client_id }
 
   describe 'index' do
-    before { login(create(:user, :client_admin)) }
-
     it '200' do
       get client_admin_work_contents_path
       expect(response).to have_http_status(:ok)
     end
   end
 
-  xdescribe 'new' do
+  describe 'new' do
     it '200' do
       get new_client_admin_work_content_path
       expect(response).to have_http_status(:ok)
     end
   end
 
-  xdescribe 'edit' do
+  describe 'edit' do
     it '200' do
       get edit_client_admin_work_content_path(work_content)
       expect(response).to have_http_status(:ok)
     end
   end
 
-  xdescribe 'create' do
+  describe 'create' do
     it '302' do
       post client_admin_work_contents_path, params: {
         work_content: {
-          name: 'テスト',
+          client_id: user.client_id,
+          name: '試験',
+          name_kana: 'テスト',
+          active: true,
         },
       }
       expect(response).to have_http_status(:found)
     end
   end
 
-  xdescribe 'update' do
+  describe 'update' do
     it '302' do
       patch client_admin_work_content_path(work_content), params: {
         work_content: {
@@ -50,7 +54,7 @@ RSpec.describe "/client_admin/work_contents", type: :request do
     end
   end
 
-  xdescribe 'destroy' do
+  describe 'destroy' do
     it '302' do
       delete client_admin_work_content_path(work_content)
       expect(response).to have_http_status(:found)
--- spec/requests/system_admin/clients_spec.rb
+++ spec/requests/system_admin/clients_spec.rb
@@ -3,32 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe '/system_admin/clients', type: :request do
+  before { login(create(:user, :system_admin)) }
+
   let!(:client) { create :client }
 
   describe 'index' do
-    before { login(create(:user, :system_admin)) }
-
     it '200' do
       get system_admin_clients_path
       expect(response).to have_http_status(:ok)
     end
   end
 
-  xdescribe 'new' do
+  describe 'new' do
     it '200' do
       get new_system_admin_client_path
       expect(response).to have_http_status(:ok)
     end
   end
 
-  xdescribe 'edit' do
+  describe 'edit' do
     it '200' do
       get edit_system_admin_client_path(client)
       expect(response).to have_http_status(:ok)
     end
   end
 
-  xdescribe 'create' do
+  describe 'create' do
     it '302' do
       post system_admin_clients_path, params: {
         client: {
@@ -39,7 +39,7 @@ RSpec.describe '/system_admin/clients', type: :request do
     end
   end
 
-  xdescribe 'update' do
+  describe 'update' do
     it '302' do
       patch system_admin_client_path(client), params: {
         client: {
@@ -50,7 +50,7 @@ RSpec.describe '/system_admin/clients', type: :request do
     end
   end
 
-  xdescribe 'destroy' do
+  describe 'destroy' do
     it '302' do
       delete system_admin_client_path(client)
       expect(response).to have_http_status(:found)
